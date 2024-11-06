d7bf6c9a5237f82cee839689ac97eaeccb3f40f9
Abudu Samadu <abudu.samadu@amalitech.com>
2024-11-06 03:32:26 +0000

    Adding week2 activities

---
+++ StringReversal.java
@@ -0,0 +1,36 @@
+
+public class StringReversal {
+
+    public static String reverseSubstring(String str, int start, int end) {
+        // Convert the string to a char array
+        char[] charArray = str.toCharArray();
+
+        // Reverse characters between start and end
+        while (start < end) {
+            // Swap characters at indexes 'start' and 'end'
+            char temp = charArray[start];
+            charArray[start] = charArray[end];
+            charArray[end] = temp;
+
+            // Move pointers towards each other
+            start++;
+            end--;
+        }
+
+        // Convert the char array back to a string
+        return new String(charArray);
+    }
+
+    public static void main(String[] args) {
+        String input = "hello";
+
+        // Reverse characters from index 1 to index 3
+        int start = 1;
+        int end = 3;
+
+        String result = reverseSubstring(input, start, end);
+
+        // Print the modified string
+        System.out.println(result);
+    }
+}
