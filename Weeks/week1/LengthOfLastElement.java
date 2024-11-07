d1e1ff41dff2b58e8f250602fc42d5c6f8360f7b
Abudu Samadu <abudu.samadu@amalitech.com>
2024-11-07 13:40:43 +0000

    Adding the metter

--- Weeks/week1/LengthOfLastElement.java
+++ Weeks/week1/LengthOfLastElement.java
@@ -3,7 +3,17 @@ public class LengthOfLastElement {
 
     public static void main(String[] args) {
         String s = "Hello World";
-        String[] words = s.split(" ");
-        System.out.println(words[words.length - 1].length());
+        int length = 0;
+        int i = s.length() - 1;
+        while (i >= 0 && s.charAt(i) == ' ') {
+            i--;
+        }
+        
+        while (i >= 0 && s.charAt(i) != ' ') {
+            length++;
+            i--;
+        }
+        
+        System.out.println("Length of the last non-empty word: " + length);
     }
 }
