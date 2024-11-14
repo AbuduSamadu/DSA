30a1ab6232e713ac7bfb05d41d97f1430c66f466
Abudu Samadu <abudu.samadu@amalitech.com>
2024-11-14 09:37:02 +0000

    linked list reversal

--- Weeks/Week2/ReverseList.java
+++ Weeks/Week2/ReverseList.java
@@ -2,4 +2,18 @@ package Weeks.Week2;
 
 public class ReverseList {
   
+  // Function to reverse a linked list  
+  public ListNode reverseList(ListNode head) {
+    ListNode prev = null;
+    ListNode current = head;
+    ListNode next;
+    while (current != null) {
+      next = current.getNext();
+      current.setNext(prev);
+      prev = current;
+      current = next;
+    }
+    head = prev;
+    return head;
+  }
 }
